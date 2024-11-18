a0f5796359cac12151a8329816a0199438db1f2c
Nihad ELBARI <e2405911@ensimlan.univ-lemans.fr>
2024-11-18 16:41:44 +0100

    affichage resultats de test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,19 +6,23 @@ import static org.junit.Assert.*;
 
 public class AppTest {
 
-    @Test(expected = LimitVisitorException.class)
-    public void testDepassementLimiteVisiteurs() throws LimitVisitorException {
-        // Création du zoo
+   @Test(expected = LimitVisitorException.class)
+    public void testDepassementLimiteVisiteurs() {
+        // Création du zoo pour le test
         Zoo zoo = new Zoo();
         zoo.setNbVisiteurMaxParSecteur(15); // Limite de 15 visiteurs
 
         // Ajout de 16 visiteurs (1 de plus que la limite pour avoir le prblm de 1 vis plus)
         for (int i = 0; i < 16; i++) {
-            zoo.nouveauVisiteur();
+            try {
+                zoo.nouveauVisiteur();
+            } catch (LimitVisitorException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
         @Test
-        public void testAjoutChienDansSecteur() throws AnimalInWrongSectorException {
+        public void testAjoutChienDansSecteur() {
             // Création d'un secteur pour les chiens
             Secteur secteurChien = new Secteur(TypeAnimal.CHIEN);
 
@@ -26,7 +30,11 @@ public class AppTest {
             Animal chien = new Chien("Buddy", TypeAnimal.CHIEN);
 
             // Ajout du chien dans le secteur
-            secteurChien.ajouterAnimal(chien);
+            try {
+                secteurChien.ajouterAnimal(chien);
+            } catch (AnimalInWrongSectorException e) {
+                throw new RuntimeException(e);
+            }
 
             // Vérification que le chien a bien été ajouté au secteur des chiens
             assertEquals(1, secteurChien.getNombreAnimaux());
